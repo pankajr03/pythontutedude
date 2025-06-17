923c1331b268b9c4d18ce904ae7dc2048a473e24
Pankaj Kumar <pankaj1211ranout@gmail.com>
2025-06-17 18:41:46 +0530

    Completed Assignment 1 - Module 2: Basic Python Concepts Task 1 and Task 2

---
+++ assignment_1_task1.py
@@ -0,0 +1,15 @@
+first_number = input('Enter the first number: ')
+second_number = input('Enter the second number: ')
+first_number = int(first_number)
+second_number = int(second_number)
+
+addition = first_number + second_number
+subtraction = first_number - second_number
+multiplication = first_number * second_number
+division = first_number / second_number
+
+print(f'Addition: {addition}')
+print(f'Subtraction: {subtraction}')
+print(f'Multiplication: {multiplication}')
+print(f'Division: {division}')
+
